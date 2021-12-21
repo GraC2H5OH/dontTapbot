e1e35059c81aa206dcf2635896f01f1b7c90458b
GraC2H5OH <82993704+GraC2H5OH@users.noreply.github.com>
2021-12-21 21:49:46 +0300

    Update donttap.py
    some spaces 
    ru2en

--- donttap.py
+++ donttap.py
@@ -1,9 +1,6 @@
-#эта говнина работает только на мониторах 16 на 9 и 1920x1080
-#скорее всего любое отклонение от этих величин сломает программу
-#и оно будет скринить с отклонением
-#y=895
-#x=1163
-#x_pad=534
+#y = 895
+#x = 1163
+#x_pad = 534
 #y_pad=266
 from numpy import *
 import pyautogui
@@ -14,33 +11,33 @@ import os
 import time
 import datetime
 from PIL import Image, ImageOps
-x_pad=636
-y_pad=260
+x_pad = 636
+y_pad = 260
 #скрин игровой области
 def scrGrab():
-    co=(x_pad+1,y_pad+1,630,630)
+    co=(x_pad + 1, y_pad + 1, 630, 630)
     im=pyautogui.screenshot(region=co)
     return im
-    #im1=pyautogui.screenshot(region=(x_pad+1,y_pad+1,630,630))
-    #im1.save(os.getcwd()+'\\full_snap__'+str(int(time.time()))+'.png','PNG')
+    #im1=pyautogui.screenshot(region=(x_pad + 1, y_pad + 1, 630, 630))
+    #im1.save(os.getcwd() + '\\full_snap__' + str(int(time.time()))+ '.png', 'PNG')
    # print(pyautogui.locateOnScreen('loa.png'))
 
-#имитация лкм
+#imitation lmb
 def leftClick():
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(.1)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
     print('click')
-#перемещение мышки в позицию
+#set mouse cursor in pos
 def mousePos(cord):
     win32api.SetCursorPos((x_pad + cord[0], y_pad + cord[1]))
-#получение координат для плиток уже юзлес
+#useless feature actually
 def get_cords():
-    x,y=win32api.GetCursorPos()
-    x= x-x_pad
-    y =y-y_pad
-    print(x,' ',y)
-#перемещение мышки в конкретную позицию плитки
+    x, y = win32api.GetCursorPos()
+    x = x - x_pad
+    y = y - y_pad
+    print(x, ' ', y)
+#set mouse cursor to tile pos
 def Game1():
     #1
     mousePos((Cord.odin1))
@@ -122,7 +119,7 @@ def Game16():
     leftClick()
     time.sleep(.1)
 class Cord:
-    odin1=(73,75)
+    odin1=(73, 75)
     odin2=(235, 76)
     odin3=(394, 79)
     odin4=(537, 77)
@@ -139,68 +136,68 @@ class Cord:
     odin15=(391, 551)
     odin16=(543, 548)
 def playGame():
-    s=scrGrab()
-    if s.getpixel(Cord.odin1)==(0,0,0):
+    s = scrGrab()
+    if s.getpixel(Cord.odin1) == (0, 0, 0):
         mousePos(Cord.odin1)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin2)==(0,0,0):
+    if s.getpixel(Cord.odin2) == (0, 0, 0):
         mousePos(Cord.odin2)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin3)==(0,0,0):
+    if s.getpixel(Cord.odin3) == (0, 0, 0):
         mousePos(Cord.odin3)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin4)==(0,0,0):
+    if s.getpixel(Cord.odin4) == (0, 0, 0):
         mousePos(Cord.odin4)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin5)==(0,0,0):
+    if s.getpixel(Cord.odin5) == (0, 0, 0):
         mousePos(Cord.odin5)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin6)==(0,0,0):
+    if s.getpixel(Cord.odin6) == (0, 0, 0):
         mousePos(Cord.odin6)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin7)==(0,0,0):
+    if s.getpixel(Cord.odin7) == (0, 0, 0):
         mousePos(Cord.odin7)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin8)==(0,0,0):
+    if s.getpixel(Cord.odin8) == (0, 0, 0):
         mousePos(Cord.odin8)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin9)==(0,0,0):
+    if s.getpixel(Cord.odin9) == (0, 0, 0):
         mousePos(Cord.odin9)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin10)==(0,0,0):
+    if s.getpixel(Cord.odin10) == (0, 0, 0):
         mousePos(Cord.odin10)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin11)==(0,0,0):
+    if s.getpixel(Cord.odin11) == (0, 0, 0):
         mousePos(Cord.odin11)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin12)==(0,0,0):
+    if s.getpixel(Cord.odin12) == (0, 0, 0):
         mousePos(Cord.odin12)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin13)==(0,0,0):
+    if s.getpixel(Cord.odin13) == (0, 0, 0):
         mousePos(Cord.odin13)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin14)==(0,0,0):
+    if s.getpixel(Cord.odin14) == (0, 0, 0):
         mousePos(Cord.odin14)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin15)==(0,0,0):
+    if s.getpixel(Cord.odin15) == (0, 0, 0):
         mousePos(Cord.odin15)
         time.sleep(.01)
         leftClick()
-    if s.getpixel(Cord.odin16)==(0,0,0):
+    if s.getpixel(Cord.odin16) == (0, 0, 0):
         mousePos(Cord.odin16)
         time.sleep(.01)
         leftClick()
@@ -211,5 +208,6 @@ def main():
 if __name__=='__main__':
     main()
 i=1
-for i in range(1,228):
+#iterations
+for i in range(1, 228):
     playGame()
